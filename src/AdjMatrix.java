9b381bd12c475b3b0a76998d4494ec3d3bfcca65
christma <liuchuanmumumu@gmail.com>
2020-01-29 11:32:32 +0800

    Add some methods

--- src/AdjMatrix.java
+++ src/AdjMatrix.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdjMatrix {
@@ -14,19 +15,59 @@ public class AdjMatrix {
         try (Scanner scanner = new Scanner(file)) {
 
             V = scanner.nextInt();
+            if (V < 0) throw new IllegalArgumentException("V must be non-negative ");
             adj = new int[V][V];
-
+            if (E < 0) throw new IllegalArgumentException("E must be non-negative ");
             E = scanner.nextInt();
 
             for (int i = 0; i < E; i++) {
                 int a = scanner.nextInt();
+                validateVertex(a);
                 int b = scanner.nextInt();
+                validateVertex(b);
+                if (a == b) throw new IllegalArgumentException("自环边");
+                if (adj[a][b] == 1) throw new IllegalArgumentException("平行边");
                 adj[a][b] = 1;
-                adj[a][b] = 1;
+                adj[b][a] = 1;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is invalid");
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public boolean hasEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v][w] == 1;
+    }
+
+    public ArrayList<Integer> adj(int v) {
+        validateVertex(v);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (adj[v][i] == 1) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public int degree(int v) {
+        return adj(v).size();
     }
 
     @Override
@@ -44,6 +85,7 @@ public class AdjMatrix {
 
     public static void main(String[] args) {
         AdjMatrix adjMatrix = new AdjMatrix("g.txt");
+        System.out.println(adjMatrix);
     }
 
 }
